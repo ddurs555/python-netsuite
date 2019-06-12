f0e8298cc1b50c27008177eda9788832da1b4e6c
ddurst <ddurst@mrosupply.com>
2019-06-12 15:26:56 -0700

    Finished updates to netsuite.api.restlet.client Still untested code

--- netsuite/api/restlet/client.py
+++ netsuite/api/restlet/client.py
@@ -8,6 +8,8 @@ import ns_config
 
 
 SIG_METHOD = oauth.SignatureMethod_HMAC_SHA1()
+
+
 def _get_params(token, consumer):
     return {
         'oauth_version': "1.0",
@@ -25,66 +27,70 @@ def _get_oauth_request(app,method, url):
         parameters=_get_params(token=app.token, consumer=app.consumer)
     )
 
-def get(payload):
-    method = 'GET'
-    app = get_next_restlet_app()
 
-    request_url = requests.Request(method,
-                                   app.url,
-                                   params=payload).prepare().url
-    req = _get_oauth_request(app=app, method='GET', url=request_url)
-
-
+def _generate_headers(app, method, url):
+    req = _get_oauth_request(app=app, method=method, url=url)
     req.sign_request(SIG_METHOD, app.consumer, app.token)
     header = req.to_header(app.account)
     headery = header['Authorization'].encode('ascii', 'ignore')
-    headerx = {"Authorization": headery, "Content-Type":"application/json"}
-    conn = requests.get(request_url,headers=headerx,)
-    print(conn.text)
+    return {
+        "Authorization": headery,
+        "Content-Type": "application/json",
+    }
+
+
+def get(payload):
+    method = 'GET'
+    app = get_next_restlet_app()
+    url = requests.Request(method,
+                           app.url,
+                           params=payload).prepare().url
+
+    conn = requests.get(url,
+                        headers=_generate_headers(app, method, url),)
+    return json.loads(conn.text)
+
 
 def post(payload):
     method = 'POST'
-app = ''
-	req = _get_oauth_request(app=app, method=method, url=app.url)
-	req.sign_request(SIG_METHOD, app.consumer, app.token)
-	header = req.to_header(app.account)
-    headery = header['Authorization'].encode('ascii', 'ignore')
-    headerx = {"Authorization": headery, "Content-Type":"application/json"}
-    conn = requests.post(app.url,headers=headerx,data=json.dumps(payload))
-    print(conn.text)
+    app = get_next_restlet_app()
+    conn = requests.post(app.url,
+                         headers=_generate_headers(app, method, app.url),
+                         data=json.dumps(payload))
+    return json.loads(conn.text)
+
 
 def put(payload):
-	method = 'PUT'
+    method = 'PUT'
+    app = get_next_restlet_app()
+    conn = requests.post(app.url,
+                         headers=_generate_headers(app, method, app.url),
+                         data=json.dumps(payload))
+    return json.loads(conn.text)
 
-	#conn = requests.put(base_url,headers=headerx,data=json.dumps(payload))
-	print(conn.text)
 
 def delete(payload):
-	method = 'DELETE'
-	'''
-	request_url = requests.Request(method,base_url,params=payload).prepare().url
-	req = oauth.Request(method=method, url=request_url, parameters=params)
+    method = 'DELETE'
+    app = get_next_restlet_app()
+    url = requests.Request(method,
+                           app.url,
+                           params=payload).prepare().url
 
-	signature_method = oauth.SignatureMethod_HMAC_SHA1()
-	req.sign_request(signature_method, consumer, token)
-	header = req.to_header(realm)
-	headery = header['Authorization'].encode('ascii', 'ignore')
-	headerx = {"Authorization": headery, "Content-Type":"application/json"}
-	conn = requests.delete(request_url,headers=headerx,)
-	print(conn.text)
-	'''
+    conn = requests.get(url, headers=_generate_headers(app, method, url), )
+    return json.loads(conn.text)
+
 
 def main():
-	payload = {
-		'custrecord_cc_image':'https://mrosupply2-dyn-images.s3.amazonaws.com/category_image/872_na6KdbH_Eahra48.jpg',
-		'name':'davids API test',
-		'custrecord_cc_description':'Take me down to the paradise city where the grass is green',
-		'custrecord_cc_short_description':'ooooh wont you please take me home',
-	}
-	get(payload={'id':'38'})
-	post(payload=payload)
-	put(payload=payload)
-	delete(payload=payload)
+    payload = {
+        'custrecord_cc_image':'https://mrosupply2-dyn-images.s3.amazonaws.com/category_image/872_na6KdbH_Eahra48.jpg',
+        'name':'davids API test',
+        'custrecord_cc_description':'Take me down to the paradise city where the grass is green',
+        'custrecord_cc_short_description':'ooooh wont you please take me home',
+    }
+    get(payload={'id':'38'})
+    post(payload=payload)
+    put(payload=payload)
+    delete(payload=payload)
 
 if __name__ == "__main__":
-	main()
+    main()
